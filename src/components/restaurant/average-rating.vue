f5406a0f43c9934881ab5ecf2503805f22b5e55e
Romain Minot <rom_ino@hotmail.fr>
2025-03-26 20:33:44 +0100

    fix(restaurant): Ensure average rating only displays when reviews are present
    - Added a computed property to check for the presence of reviews before displaying the average rating component.
    - Updated the template to conditionally render the VRating component based on the availability of reviews

--- src/components/restaurant/average-rating.vue
+++ src/components/restaurant/average-rating.vue
@@ -5,11 +5,13 @@ const props = defineProps<{
   reviews?: Review[]
 }>();
 
+const hasReviews = computed(() => props.reviews && props.reviews.length > 0);
 const averageRating = computed(() => useAverageRating(props.reviews));
 </script>
 
 <template>
   <VRating
+    v-if="hasReviews"
     :length="5"
     :size="32"
     :model-value="averageRating"
