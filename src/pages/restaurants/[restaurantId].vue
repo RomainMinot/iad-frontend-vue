02dcf3299b8132ac19dc96b01d711f43b7068843
Romain Minot <rom_ino@hotmail.fr>
2025-03-27 14:44:20 +0100

    refactor(restaurant): Update grid layout for restaurant details page
    - Modified the grid layout to be responsive, allowing for a single column on smaller devices and maintaining a two-column layout on medium and larger devices.
    - Removed the TODO alert regarding layout adjustments for small devices from the template and snapshots.

--- src/pages/restaurants/[restaurantId].vue
+++ src/pages/restaurants/[restaurantId].vue
@@ -11,7 +11,7 @@ const { data: restaurant, isError } = useFetchRestaurant({ restaurantId: params.
     <br>
     You should fix that
   </VAlert>
-  <div v-else class="grid grid-cols-[minmax(0,_1fr)_16rem] gap-6">
+  <div v-else class="grid grid-cols-1 md:grid-cols-[minmax(0,_1fr)_16rem] gap-6">
     <VCard v-if="restaurant">
       <VImg
         v-for="photo in restaurant.photos"
@@ -38,9 +38,6 @@ const { data: restaurant, isError } = useFetchRestaurant({ restaurantId: params.
       </VCardText>
     </VCard>
     <aside>
-      <VAlert type="warning">
-        TODO: this should go under the company card on small device
-      </VAlert>
       <ul class="pa-0">
         <RestaurantReview />
       </ul>
